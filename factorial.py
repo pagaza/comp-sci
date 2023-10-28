321dc06c3675732b5c464ab18d597d9920a197f0
pagaza <sophieplouin@icloud.com>
2023-10-27 22:42:28 -0600

    hw

---
+++ factorial.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+i = num 
+
+while i > 1:
+    i -= 1
+    num = num * (i)
+
+print("The factorial is ", num)
---
+++ whileLoop.py
@@ -0,0 +1,5 @@
+num = 30
+
+while num <= 50:
+    print(num)
+    num = num + 3
