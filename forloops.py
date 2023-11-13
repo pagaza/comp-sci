f08082412b59d209879a7e3cac976cabe53746cc
pagaza <sophieplouin@icloud.com>
2023-11-13 13:46:11 -0600

    hw

--- forloops.py
+++ forloops.py
@@ -1,19 +1,49 @@
 # Exercise 1
 
-for x in range(20,1,-5):
-    print(x)
+n = int(input("Please enter a number: "))
+s = int(input("Please enter steps: "))
+
+for i in range (1, n + 1, s):
+    print(i)
 
 # Exercise 2
 
-num = int(input("Enter a number between 5 and 12:"))
+n = int(input("Please enter a number: "))
 
-while True: 
+for j in range (n, -1, -1):
+    print(j)
 
-    if num >= 5 and num <= 12:
-        for x in range(1,11):
-            print(num * x)
+print("Done.")
 
-        break
+# Exercise 3
 
-    else:
-        num = int(input("Enter a number between 5 and 12:"))
+n = int(input("Please enter a number: "))
+op = str(input("Please choose a type of operation (add, substract, multiply): "))
+
+if op.lower() == "add":
+
+    for i in range (1, n +1):
+
+        if i%2 == 0:
+            print(i, "Even")
+
+        else:
+            print(i, "Odd")
+
+elif op.lower() == "substract":
+    for j in range (n, 0, -1):
+        
+        if j%2 == 0:
+            print(j, "Even")
+
+        else:
+            print(j, "Odd")
+
+elif op.lower() == "multiply":
+
+    for k in range(n, 1, -1):
+
+        k -= 1
+        n = n*k
+
+    print(n)
